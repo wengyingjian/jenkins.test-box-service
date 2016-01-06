8fa21c849d95813afb39723101edaebc8cf113ae
wengyingjian <q291611265@gmail.com>
2016-01-06 12:25:47 +0800

    a

--- box-service/src/main/java/com/store59/box/BoxServiceApplication.java
+++ box-service/src/main/java/com/store59/box/BoxServiceApplication.java
@@ -1,8 +1,5 @@
 package com.store59.box;
 
-import java.net.MalformedURLException;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,18 +12,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
-import com.caucho.hessian.client.HessianProxyFactory;
-import com.store59.box.remoting.UserService;
-
 @EnableCaching
 @SpringBootApplication
 @ComponentScan(basePackages = "com.store59")
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
-@PropertySource("classpath:boxservice.properties")
+//@PropertySource("classpath:boxservice.properties")
 public class BoxServiceApplication extends SpringBootServletInitializer {
-
-    @Value("${userservice.url}")
-    private String userServiceUrl;
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -35,16 +26,6 @@ public class BoxServiceApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BoxServiceApplication.class, args);
-    }
-
-    @Bean
-    UserService userService() throws MalformedURLException {
-        HessianProxyFactory hessianProxyFactory = new HessianProxyFactory();
-        hessianProxyFactory.setOverloadEnabled(true);
-        if (!userServiceUrl.endsWith("/")) {
-            userServiceUrl = userServiceUrl + "/";
-        }
-        return (UserService) hessianProxyFactory.create(UserService.class, userServiceUrl + "user");
     }
 
     @Bean
